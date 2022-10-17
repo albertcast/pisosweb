dec729f933b1e89f031b3956c38c12d4ab0c4827
albertcast <48439879+albertcast@users.noreply.github.com>
2022-10-17 17:37:22 +0200

    inicio

---
+++ src/main/java/com/example/pisosweb/PisoswebApplication.java
@@ -0,0 +1,13 @@
+package com.example.pisosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisoswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisoswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pisosweb/PisoswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pisosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisoswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
