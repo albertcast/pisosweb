ebd670ae4efe8d1ceb1c5a328e0f74e08400f2a4
albertcast <48439879+albertcast@users.noreply.github.com>
2022-11-14 17:30:50 +0100

    deploy

--- src/main/java/com/example/pisosweb/controller/UserController.java
+++ src/main/java/com/example/pisosweb/controller/UserController.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = {"http://localhost:8080","https://pisoswebcliente.herokuapp.com"})
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
